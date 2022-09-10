ca0ed81c76cf7d9c4103172a66df95bd8a12cf3e
YogXat <vare.140201@gmail.com>
2022-09-10 00:56:03 -0500

    code formatting

--- main.js
+++ main.js
@@ -1,94 +1,87 @@
 document.addEventListener("DOMContentLoaded", () => {
+   //---------------------------------------------------------------------------
 
-//---------------------------------------------------------------------------
+   // Elementos del fomulario
+   const descriptionForm = document.querySelector(".form-description");
+   const titleForm = document.querySelector(".form-title");
+   const indexHTML = document.querySelector(".index");
+   //Sección de notas
+   const noteDiv = document.querySelector(".list-notes");
+   //Botones
+   const noteForm = document.querySelector(".submit-note");
+   const updateForm = document.querySelector(".update-note");
+   const deleteBtn = document.getElementsByClassName("note-delete");
+   const updateBtn = document.getElementsByClassName("note-edit");
+   //Almacenamiento local
+   let noteStorage = window.localStorage;
 
-    // Elementos del fomulario
-    const descriptionForm = document.querySelector(".form-description")
-    const titleForm = document.querySelector(".form-title")
-    const indexHTML = document.querySelector(".index")
-    //Sección de notas
-    const noteDiv = document.querySelector(".list-notes")
-    //Botones 
-    const noteForm = document.querySelector(".submit-note");
-    const updateForm = document.querySelector(".update-note")
-    const deleteBtn = document.getElementsByClassName("note-delete")
-    const updateBtn = document.getElementsByClassName("note-edit")
-    //Almacenamiento local
-    let noteStorage = window.localStorage;
+   //----------------------------------------------------------------------------
 
-//----------------------------------------------------------------------------
+   // Carga notas ya almacenadas
+   if (noteStorage != null) {
+      for (let i = 0; i < noteStorage.length; i++) {
+         let note = JSON.parse(noteStorage.getItem(localStorage.key(i)));
+         addNotes(note["title"], note["description"]);
+      }
+   }
+   // Agregar notas
+   noteForm.addEventListener("click", () => {
+      let note = {
+         title: titleForm.value,
+         description: descriptionForm.value,
+      };
 
-    // Carga notas ya almacenadas
-    if(noteStorage != null) {
-        for ( let i = 0; i < noteStorage.length; i++) {
-            let note = JSON.parse(noteStorage.getItem(localStorage.key(i)))
-            addNotes(note["title"], note["description"])          
-        }
-    } 
-    // Agregar notas
-    noteForm.addEventListener("click", () => {
+      addNotes(note["title"], note["description"]);
 
-        let note = {
-            title: titleForm.value,
-            description: descriptionForm.value
-        }
-        
-        addNotes(note["title"], note["description"])
+      let storageLength = noteStorage.length;
+      noteStorage.setItem(storageLength++, JSON.stringify(note));
+      location.reload();
+   });
+   // Actualizar notas
+   updateForm.addEventListener("click", () => {
+      let note = {
+         title: titleForm.value,
+         description: descriptionForm.value,
+      };
 
-        let storageLength = noteStorage.length
-        noteStorage.setItem(storageLength++, JSON.stringify(note))
-        location.reload()
-    })
-    // Actualizar notas
-    updateForm.addEventListener("click", () => {
-        let note = {
-            title: titleForm.value,
-            description: descriptionForm.value
-        }
+      noteStorage.setItem(indexHTML.innerHTML, JSON.stringify(note));
+      location.reload();
+   });
+   // Elimina notas
+   for (let i = 0; i < deleteBtn.length; i++) {
+      deleteBtn[i].addEventListener("click", () => {
+         deleteNotes(deleteBtn[i]);
+         location.reload();
+      });
+   }
 
-        noteStorage.setItem(indexHTML.innerHTML, JSON.stringify(note))
-        location.reload()
+   // Selecciona nota a editar
+   for (let i = 0; i < updateBtn.length; i++) {
+      updateBtn[i].addEventListener("click", () => {
+         updateNote(updateBtn[i]);
+      });
+   }
 
-    })
-    // Elimina notas
-    for (let i = 0; i < deleteBtn.length; i++) {
-        deleteBtn[i].addEventListener('click', () => {
-            deleteNotes(deleteBtn[i])
-            location.reload()
-        })
-    }
+   //----------------------------------------------------------------------------
 
-    // Selecciona nota a editar
-    for (let i = 0; i < updateBtn.length; i++) {
-        updateBtn[i].addEventListener('click', () => {
-            updateNote(updateBtn[i])
-        })
-    }
+   // Funciones principales
+   function updateNote(i) {
+      let index = Array.from(updateBtn).indexOf(i);
+      let noteIndex = noteStorage.key(index);
+      let note = JSON.parse(noteStorage.getItem(noteIndex));
 
+      titleForm.value = note.title;
+      descriptionForm.innerHTML = note.description;
+      indexHTML.innerHTML = noteIndex;
+   }
 
-//----------------------------------------------------------------------------
+   function deleteNotes(i) {
+      let index = Array.from(deleteBtn).indexOf(i);
+      noteStorage.removeItem(noteStorage.key(index));
+   }
 
-
-    // Funciones principales
-    function updateNote(i){
-        let index = Array.from(updateBtn).indexOf(i)
-        let noteIndex = noteStorage.key(index)
-        let note = JSON.parse(noteStorage.getItem(noteIndex))
-
-
-        titleForm.value = note.title
-        descriptionForm.innerHTML = note.description
-        indexHTML.innerHTML = noteIndex        
-    }
-
-
-    function deleteNotes(i) {
-        let index = Array.from(deleteBtn).indexOf(i)
-        noteStorage.removeItem(noteStorage.key(index))
-    }
-    
-    function addNotes(title, description) {
-        const noteHTML = `
+   function addNotes(title, description) {
+      const noteHTML = `
             <div class="note">
                 <div class="note-information">
                     <h1 class="note-title">${title}</h1>
@@ -106,8 +99,6 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
         `;
 
-        noteDiv.insertAdjacentHTML("beforeend", noteHTML);
-    }
-    
-
-})
+      noteDiv.insertAdjacentHTML("beforeend", noteHTML);
+   }
+});
